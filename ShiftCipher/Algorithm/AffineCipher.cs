d75236b164af232053b4380d5ab8c6849fd8ef9e
Muhammad Afiq <blackzafiqz@gmail.com>
2023-07-12 09:03:24 +0800

    Remove comments

--- ShiftCipher/Algorithm/AffineCipher.cs
+++ ShiftCipher/Algorithm/AffineCipher.cs
@@ -21,21 +21,13 @@ namespace ShiftCipher.Algorithm
             {
                 if (char.IsLetter(c))
                 {
-                    // Convert the character to uppercase
                     char upperC = char.ToUpper(c);
-
-                    // Apply the affine cipher formula: E(x) = (ax + b) mod 26
                     int encryptedValue = mod((a * (upperC - 'A') + b), 26);
-
-                    // Convert the encrypted value back to a character
                     char encryptedChar = (char)(encryptedValue + 'A');
-
-                    // Append the encrypted character to the ciphertext
                     cipherText += encryptedChar;
                 }
                 else
                 {
-                    // If the character is not a letter, simply append it to the ciphertext
                     cipherText += c;
                 }
             }
@@ -47,7 +39,7 @@ namespace ShiftCipher.Algorithm
         {
             string plainText = "";
 
-            // Calculate the modular inverse of 'a'
+            
             int modInverse = -1;
             for (int i = 0; i < 26; i++)
             {
@@ -68,21 +60,13 @@ namespace ShiftCipher.Algorithm
             {
                 if (char.IsLetter(c))
                 {
-                    // Convert the character to uppercase
                     char upperC = char.ToUpper(c);
-
-                    // Apply the affine cipher formula: D(x) = (a^(-1) * (x - b)) mod 26
                     int decryptedValue = mod((modInverse * (upperC - 'A' - b)), 26);
-
-                    // Convert the decrypted value back to a character
                     char decryptedChar = (char)(decryptedValue + 'A');
-
-                    // Append the decrypted character to the plaintext
                     plainText += decryptedChar;
                 }
                 else
                 {
-                    // If the character is not a letter, simply append it to the plaintext
                     plainText += c;
                 }
             }
